6eff617b4e3fbec07396208bf4cf882fe15f8234
Tóth Kristóf <tothkristof31@edu.bme.hu>
2023-10-28 18:07:31 +0200

    promisify and predict

--- src/prediction/prediction.service.ts
+++ src/prediction/prediction.service.ts
@@ -7,12 +7,13 @@ export class PredictionService {
   private pythonScriptPath = 'python/child.py';
 
   public async executePythonScript(ticker: string): Promise<number> {
-    const ex = util.promisify(exec);
+    const asyncExec = util.promisify(exec);
     let prediction = -1;
-    const childResult: { stdout: string; stderr: string } = await ex(
+    const childResult: { stdout: string; stderr: string } = await asyncExec(
       `C:\\Users\\Dell\\AppData\\Local\\Programs\\Python\\Python311\\python.exe ${this.pythonScriptPath} ${ticker}`,
     );
     prediction = parseFloat(childResult.stdout);
+    console.log(prediction);
     return prediction;
   }
 }
